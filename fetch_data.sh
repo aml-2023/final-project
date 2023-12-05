41d2cc826c3e8257b597b883f048ec01e849eb05
jonasbarth <jonas.barth.95@gmail.com>
2023-12-05 12:21:07 +0100

    Add possibiltiy for using COCO or YOLO dataset when fetching

--- fetch_data.sh
+++ fetch_data.sh
@@ -1,10 +1,43 @@
 #!/bin/bash
 
-# Define the URL with the appropriate API key
-url="https://universe.roboflow.com/ds/vunUxYLq9j?key=jDCfG8KbT0"
-
-# Define the output folder name
+# Default values
+dataset_type="coco"
 output_folder="data"
+
+# Parse command-line arguments
+while [[ $# -gt 0 ]]; do
+    key="$1"
+
+    case $key in
+        -t|--type)
+            dataset_type="$2"
+            shift
+            shift
+            ;;
+        -o|--output)
+            output_folder="$2"
+            shift
+            shift
+            ;;
+        *)
+            echo "Unknown option: $1"
+            exit 1
+            ;;
+    esac
+done
+
+# Validate dataset type
+if [[ ! "$dataset_type" =~ ^(coco|yolo)$ ]]; then
+    echo "Invalid dataset type. Use 'coco' or 'yolo'."
+    exit 1
+fi
+
+# Define URLs based on the dataset type
+if [ "$dataset_type" == "coco" ]; then
+    url="https://universe.roboflow.com/ds/vunUxYLq9j?key=jDCfG8KbT0"
+elif [ "$dataset_type" == "yolo" ]; then
+    url="https://universe.roboflow.com/ds/UoC75yslyT?key=V3X5ZOBCmH"
+fi
 
 echo "Downloading data"
 # Download the 2data2 using curl, save it to roboflow.zip
